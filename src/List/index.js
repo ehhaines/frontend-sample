b97be5c1003736e3ad5790a2007a394a8d636e37
ehhaines <haines.e@northeastern.edu>
2022-11-08 13:25:09 -0500

    Complete styling for list screen

--- src/List/index.js
+++ src/List/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import Member from "./member";
 import members from "./members.json";
+import "./index.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const ListComponent = () => {
   return(
-    <div>
-      <div className="text-primary fs-3 w-100 text-end">+</div>
-      <h2 className="text-dark m-0">Team members</h2>
-      <div className="text-secondary fs-5">You have {members.length} team members.</div>
-      <ul>
+    <div className="eh-list-component">
+      <div className="eh-border-bottom-2 pb-4">
+        <div className="fs-3 w-100 text-end">
+          <button className="text-primary border-0 bg-transparent"><FontAwesomeIcon icon={faPlus}/></button>
+        </div>
+        <h2 className="text-dark m-0">Team members</h2>
+        <div className="text-secondary fs-5">You have {members.length} team members.</div>
+      </div>
+      <ul className="list-group">
         {members.map(mem => 
           <Member mem={mem}/>)
         }
--- src/List/member.js
+++ src/List/member.js
@@ -1,11 +1,16 @@
 import React from "react";
+import "./index.css";
 
 const Member = ({mem}) => {
   return(
-    <li className="row">
-      <img src="/images/profile.png" className="rounded-circle col-2 img-responsive" alt="Not Found"/>
-      <div className="col">
-        <div>{mem.firstName} {mem.lastName}{mem.isAdmin ? " (admin)" : null}</div>
+    <li className="list-group-item border-top-0 border-start-0 border-end-0 eh-border-bottom">
+      <div className="row py-2">
+        <img src="/images/profile.png" className="rounded-circle col-3 mt-1 p-0 eh-profile-pic" alt="Not Found"/>
+        <div className="col">
+          <div>{mem.firstName} {mem.lastName}{mem.isAdmin ? " (admin)" : null}</div>
+          <div className="text-muted">{mem.phone}</div>
+          <div className="text-muted">{mem.email}</div>
+        </div>
       </div>
     </li>
   );
